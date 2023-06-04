9ed71019bfe48d4fcc25f7869b27710f9c9f4e01
Sanjay Kumar <72185623+sanjayhjp121@users.noreply.github.com>
2023-06-05 00:20:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.use("/contact",contactRouter)
 
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*")
+    
+    res.header(
+        "'Access-Control-Allow-Methods",
+        "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+        );
+        
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+    next() ;
 })
 
 
